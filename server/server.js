5f6b558e64895b50439b953261345764a07ab2a6
ayush <ayush.kumar01012003@gmail.com>
2024-08-10 12:12:27 +0530

    connected mongodb2

--- server/server.js
+++ server/server.js
@@ -7,12 +7,13 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
-
-
 app.use(cors({
-    origin: 'http://localhost:3000' // Adjust this to your frontend URL
+    origin: 'http://localhost:3000', // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true // Allow cookies and authentication headers
 }));
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", authRoutes)
