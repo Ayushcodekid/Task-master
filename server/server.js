8ebcc11ad6a91df6368e631a8c7933336c1223bc
ayush <ayush.kumar01012003@gmail.com>
2024-08-10 13:26:40 +0530

    connect db

--- server/server.js
+++ server/server.js
@@ -7,13 +7,24 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: 'http://localhost:3000', // Replace with your frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true // Allow cookies and authentication headers
-}));
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://singular-crumble-3b2d78.netlify.app'
+  ];
 
-app.use(cors());
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
+
+
 app.use(express.json());
 
 app.use("/api", authRoutes)
