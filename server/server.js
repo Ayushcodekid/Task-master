31c8185f0f13074552f8fb8cd8db542a6fbc54c9
ayush <ayush.kumar01012003@gmail.com>
2024-08-10 15:52:31 +0530

    fixed mobile view

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://codertodolist.netlify.app/'
+    'https://codertodoapp.netlify.app'
   ];
 
   app.use(cors({
