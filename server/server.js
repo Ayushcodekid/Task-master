560eedc90cdc65e3c123b0c02a7c60dcbb3884e8
Ayushcodekid <ayush0101works@gmail.com>
2024-10-17 12:42:14 +0530

    Port changed

--- server/server.js
+++ server/server.js
@@ -29,36 +29,35 @@
 
 
 
+    const express = require('express');
+    const cors = require('cors');
+    const sequelize = require('./db');
+    const authRoutes = require('./routes/authRoutes');
+    require('dotenv').config();
 
+    const app = express();
 
-const express = require('express');
-const cors = require('cors');
-const sequelize = require('./db');
-const authRoutes = require('./routes/authRoutes');
-require('dotenv').config();
+    app.use(cors());
+    app.use(express.json());
 
-const app = express();
+    // Register the routes
+    app.use('/api', authRoutes); // Use '/api' as the base path
 
-app.use(cors());
-app.use(express.json());
-
-// Register the routes
-app.use('/api', authRoutes); // Use '/api' as the base path
-
-// Sync the database and start the server
-sequelize.sync({ alter: true }) // Set `alter: true` to automatically update tables if there are any changes in models
-    .then(() => {
-        app.listen(process.env.PORT || 7000, () => {
-            console.log(`Server is running on port ${process.env.PORT || 5000}`);
+    // Sync the database and start the server
+    sequelize.sync({ alter: true }) // Set `alter: true` to automatically update tables if there are any changes in models
+        .then(() => {
+            console.log('Connected to Neon DB');
+            app.listen(process.env.PORT || 8000, () => {
+                console.log(`Server is running on port ${process.env.PORT || 5000}`);
+            });
+        })
+        .catch(err => {
+            console.error('Unable to connect to the database:', err);
+            process.exit(1); // Exit process with failure code
         });
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-        process.exit(1); // Exit process with failure code
-    });
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-});
+    // Error handling middleware
+    app.use((err, req, res, next) => {
+        console.error(err.stack);
+        res.status(500).send('Something went wrong!');
+    });
